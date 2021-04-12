18421ef3fd709b43573d1f5241702ec0f3d2c0d5
ivagorRetrocomp <81841888+ivagorRetrocomp@users.noreply.github.com>
2021-04-12 20:05:21 +0500

    Create unlzsa1_fast.asm

---
+++ 8080/unlzsa1_fast.asm
@@ -0,0 +1,140 @@
+;Speed-optimized LZSA1 Intel 8080 decoder version by Ivan Gorodetsky
+;Based on LZSA1 decompressor by spke
+;input: 	hl=compressed data start
+;			de=uncompressed destination start
+;v 1.0 - 2019-09-15
+;v 1.1 - 2019-10-27 (-4 bytes and self-modifying code removed)
+;v 1.2 - 2021-02-23 (-25 bytes and slightly faster)
+;v 1.3 - 2021-02-25 (-6 bytes, tnanks to Improver)
+;v 1.4 - 2021-02-25 (+12 bytes, bug fix)
+;
+;compress forward with <-f1 -r> options
+;171 bytes - forward version
+;
+;compress backward with <-f1 -r -b> options
+;176 bytes - backward version
+;
+;Compile with The Telemark Assembler (TASM) 3.2
+;
+;  LZSA compression algorithms are (c) 2019 Emmanuel Marty,
+;  see https://github.com/emmanuel-marty/lzsa for more information
+;
+;  This software is provided 'as-is', without any express or implied
+;  warranty.  In no event will the authors be held liable for any damages
+;  arising from the use of this software.
+;
+;  Permission is granted to anyone to use this software for any purpose,
+;  including commercial applications, and to alter it and redistribute it
+;  freely, subject to the following restrictions:
+;
+;  1. The origin of this software must not be misrepresented; you must not
+;     claim that you wrote the original software. If you use this software
+;     in a product, an acknowledgment in the product documentation would be
+;     appreciated but is not required.
+;  2. Altered source versions must be plainly marked as such, and must not be
+;     misrepresented as being the original software.
+;  3. This notice may not be removed or altered from any source distribution.
+
+;#DEFINE BACKWARD_DECOMPRESS
+
+#IFNDEF BACKWARD_DECOMPRESS
+
+.DEFINE NEXT_HL inx h
+.DEFINE ADD_OFFSET xchg\ dad d
+.DEFINE NEXT_DE inx d
+
+#ELSE
+
+.DEFINE NEXT_HL dcx h
+.DEFINE ADD_OFFSET xchg\ mov a,e\ sub l\ mov l,a\ mov a,d\ sbb h\ mov h,a
+.DEFINE NEXT_DE dcx d
+
+#ENDIF 
+
+
+unlzsa1:
+			mvi b,0\ jmp ReadToken
+NoLiterals:
+			xra m
+			push d\ NEXT_HL\ mov e,m\ jm LongOffset
+ShortOffset:
+			mvi d,0FFh\ adi 3\ cpi 15+3\ jnc LongerMatch
+CopyMatch:
+			mov c,a
+CopyMatch_UseC:
+			NEXT_HL\ xthl
+			ADD_OFFSET
+			mov a,m\ NEXT_HL\ stax d\ NEXT_DE\ dcx b
+			mov a,m\ NEXT_HL\ stax d\ NEXT_DE\ dcx b
+			dcx b
+			inr c
+BLOCKCOPY1:
+			mov a,m
+			stax d
+			NEXT_HL
+			NEXT_DE
+			dcr c
+			jnz BLOCKCOPY1
+			xra a
+			ora b
+			jz $+7
+			dcr b
+			jmp BLOCKCOPY1
+			pop h
+ReadToken:
+			mov a,m\ ani 70h\ jz NoLiterals 
+			cpi 70h\ jz MoreLiterals
+			rrc\ rrc\ rrc\ rrc
+			mov c,a
+			mov b,m
+			NEXT_HL
+			mov a,m
+			stax d
+			NEXT_HL
+			NEXT_DE
+			dcr c
+			jnz $-5
+			push d\ mov e,m
+			mvi a,8Fh\ ana b\ mvi b,0\ jp ShortOffset
+LongOffset:
+			NEXT_HL\ mov d,m
+			adi -128+3\ cpi 15+3\ jc CopyMatch
+LongerMatch:
+			NEXT_HL\ add m\ jnc CopyMatch
+			mov b,a\ NEXT_HL\ mov c,m\ jnz CopyMatch_UseC
+			NEXT_HL\ mov b,m
+			mov a,b\ ora c\ jnz CopyMatch_UseC
+			pop d\ ret
+MoreLiterals:		
+			xra m
+			push psw
+			mvi a,7\ NEXT_HL\ add m\ jc ManyLiterals
+CopyLiterals:
+			mov c,a
+CopyLiterals_UseC:
+			NEXT_HL
+			dcx b
+			inr c
+BLOCKCOPY2:
+			mov a,m
+			stax d
+			NEXT_HL
+			NEXT_DE
+			dcr c
+			jnz BLOCKCOPY2
+			xra a
+			ora b
+			jz $+7
+			dcr b
+			jmp BLOCKCOPY2
+			pop psw
+			push d\ mov e,m
+			jp ShortOffset
+			jmp LongOffset
+
+ManyLiterals:
+			mov b,a\ NEXT_HL\ mov c,m\ jnz CopyLiterals_UseC
+			NEXT_HL\ mov b,m\ jmp CopyLiterals_UseC
+
+
+			.end
